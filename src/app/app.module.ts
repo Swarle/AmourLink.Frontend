2cd0b02eb1b1e428e8ef5f977877a7961d40ed77
Swarle <bogdanvalman@gmail.com>
2024-05-01 20:33:50 +0300

    AML-17 Added google auth button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { MainPageComponent } from './components/main-page/main-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import {ModalModule} from "ngx-bootstrap/modal";
+import {ReactiveFormsModule} from "@angular/forms";
+import {
+  GoogleLoginProvider,
+  GoogleSigninButtonModule,
+  SocialAuthServiceConfig,
+  SocialLoginModule
+} from "@abacritt/angularx-social-login";
 
 @NgModule({
   declarations: [
@@ -19,9 +26,31 @@ import {ModalModule} from "ngx-bootstrap/modal";
     AppRoutingModule,
     BrowserAnimationsModule,
     ModalModule.forRoot(),
+    ReactiveFormsModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule
 
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('902213864397-r88n97ocfudhkgv4vagle3tbmrmve9dc.apps.googleusercontent.com'),
+
+          },
+        ],
+        onError: (err) => {
+
+        },
+      } as SocialAuthServiceConfig,
+    },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GoogleLoginProvider, SocialAuthService} from "@abacritt/angularx-social-login";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-  test: boolean = false;
+export class LoginComponent implements OnInit{
+
+  constructor(private authService: SocialAuthService) {
+  }
+
+  ngOnInit(): void {
+        this.authService.authState.subscribe((user) => {
+
+        })
+    }
+
+
 }
