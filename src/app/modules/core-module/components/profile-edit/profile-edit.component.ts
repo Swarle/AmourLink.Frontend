8fa94a0968386ed3c109bd21cdb2ea8303cdcf12
Swarle <bogdanvalman@gmail.com>
2024-06-06 17:14:46 +0300

    AML-92 Add a section with adding a photo, deliting, and changing its position

--- src/app/modules/core-module/components/profile-edit/profile-edit.component.ts
+++ src/app/modules/core-module/components/profile-edit/profile-edit.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, TemplateRef, ViewEncapsulation} from '@angular/core';
 import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Picture} from "../../../../models/userModels/picture";
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-profile-edit',
@@ -15,6 +17,7 @@ export class ProfileEditComponent implements OnInit{
   basicInfoInputValidationMap: Map<string, string> = new Map([
     ["required", "Імейл обов'язковий"]
   ]);
+  pictureFile?: File;
 
   constructor(private modalService: BsModalService,private formBuilder: FormBuilder,
               public bsModalRef: BsModalRef) {
@@ -22,7 +25,29 @@ export class ProfileEditComponent implements OnInit{
       firstName: "Oleg",
       lastName: 'Petrov',
       age: 24,
-      gender: 'Чоловік'
+      gender: 'Чоловік',
+      pictures: [{
+        id: 'adfdfdas',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/15.jpg',
+        position: 0
+      },
+      {
+        id: 'adfvbxczfas',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/13.jpg',
+        position: 1
+      },
+      {
+        id: 'adfasdf',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/1.jpg',
+        position: 2
+      },
+      {
+        id: 'adfff',
+        pictureUrl: 'https://randomuser.me/api/portraits/men/4.jpg',
+        position: 3
+      }
+      ] as Picture[],
+
     }
   }
 
@@ -45,5 +70,46 @@ export class ProfileEditComponent implements OnInit{
   submitBasicInfoModal(){
     this.basicInfoModalRef?.hide();
     //TODO: Send group info when route will be ready
+  }
+  get getRemainingPhotosCount(): number[]{
+    if(this.profile.pictures.length < 6)
+      return Array(6 - this.profile.pictures.length).fill(0);
+    return Array(0);
+  }
+
+  onFileSelected(event: any) {
+    //TODO: Send request when will be ready
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const newPicture: Picture = {
+          id: 'test',
+          pictureUrl: e.target.result,
+          position: this.profile.pictures.length
+        };
+        this.profile.pictures.push(newPicture);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  drop(event: CdkDragDrop<any>) {
+    //TODO: Send request that chandes position
+    event.container.data.item.position = event.previousContainer.data.index;
+    event.previousContainer.data.item.position = event.container.data.index;
+    this.profile.pictures[event.previousContainer.data.index] = event.container.data.item;
+    this.profile.pictures[event.container.data.index] = event.previousContainer.data.item;
+  }
+
+  removePicture(id: string){
+    //TODO: Send request
+    this.profile.pictures = this.profile.pictures.filter((picture: Picture) => picture.id !== id);
+
+    this.profile.pictures.forEach((picture: Picture, index: number) => {
+      picture.position = index;
+    });
+
+    console.log(this.profile.pictures)
   }
 }
